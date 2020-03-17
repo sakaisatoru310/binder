4ff1bb89784fb1beabea01e24824f12bae2bbe12
sakaisatoru310 <tuba310@gmail.com>
2020-03-17 11:00:57 +0900

    binderの実装

---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  root to: "blog#index"
+end
---
+++ db/migrate/20200316035054_create_blogs.rb
@@ -0,0 +1,12 @@
+class CreateBlogs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :blogs do |t|
+      t.string :name
+      t.string :image
+      t.text :title
+      t.text :content
+      t.text :comment
+      t.timestamps
+    end
+  end
+end
