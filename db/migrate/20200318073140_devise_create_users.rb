63367ca02624e1e36cc88c70caeb17fb28bb98b9
sakaisatoru310 <tuba310@gmail.com>
2020-03-29 13:44:21 +0900

    Update 20200318073140_devise_create_users.rb

--- db/migrate/20200318073140_devise_create_users.rb
+++ db/migrate/20200318073140_devise_create_users.rb
@@ -43,4 +43,5 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
+  
 end
