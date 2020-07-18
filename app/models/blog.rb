24e9a283f20beeaf2eeca4bae16f451ea22a41a2
sakaisatoru310 <tuba310@gmail.com>
2020-07-18 16:10:36 +0900

    検索バーの調整

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,8 +1,6 @@
 class Blog < ApplicationRecord
   validates :title, :content, presence: true
-
   mount_uploader :image, ImageUploader
-  
   belongs_to :user
   def self.search(search)
     return Blog.all unless search
